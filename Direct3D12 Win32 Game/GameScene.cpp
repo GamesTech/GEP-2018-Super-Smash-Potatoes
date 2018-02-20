4a4e7128231371bb632a066ab601ff4f463e0bb6
Sam Hibb <samhibbert13@yahoo.co.uk>
2018-02-20 14:56:59 +0000

    Adding GameScene Template

---
+++ Direct3D12 Win32 Game/GameScene.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "GameScene.h"
+
+
+GameScene::GameScene()
+{
+}
+
+
+GameScene::~GameScene()
+{
+}
+
+void GameScene::init()
+{
+}
+
+void GameScene::update()
+{
+}
+
+void GameScene::render()
+{
+}
